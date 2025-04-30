0fd74bfea39b59cc903c6b9b547d2efff6e329f9
Francisco <fp226766@gmail.com>
2025-04-30 23:04:38 +0100

    Added the model for the emotion entry

---
+++ MindSpace/src/mindspace/model/EmotionEntry.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mindspace.model;
+
+/**
+ *
+ * @author fp226
+ */
+
+import java.time.LocalDate;
+
+public class EmotionEntry {
+    private LocalDate date;
+    private String emotion;
+    private String note;
+
+    public EmotionEntry(LocalDate date, String emotion, String note) {
+        this.date = date;
+        this.emotion = emotion;
+        this.note = note;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public String getNote() {
+        return note;
+    }
+}
