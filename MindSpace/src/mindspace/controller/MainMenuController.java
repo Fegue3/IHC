94773ea00dd1857f9a766070123f8175bd2b7fc8
Francisco <fp226766@gmail.com>
2025-05-17 01:51:39 +0100

    Added connection to the other views

--- MindSpace/src/mindspace/controller/MainMenuController.java
+++ MindSpace/src/mindspace/controller/MainMenuController.java
@@ -4,23 +4,55 @@
  */
 package mindspace.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Sofia Costa
  */
-public class MainMenuController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+public class MainMenuController implements Initializable {
+    @FXML
+    private ImageView imgRegistar;
+
+    @FXML
+    private ImageView imgCalendario;
+    
+    
+    @FXML
+    private void abrirRegisto() {
+        trocarCena("RegisterView.fxml");
+    }
+
+    @FXML
+    private void abrirCalendario() {
+        trocarCena("History.fxml");
+    }
+    
+    private void trocarCena(String nomeFxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mindspace/view/" + nomeFxml));
+            Parent root = loader.load();
+            Stage stage = (Stage) imgRegistar.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
     
