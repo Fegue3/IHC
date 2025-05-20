61564308c2e0a0a68d6ba384c0e23f416cc5d08a
Francisco <fp226766@gmail.com>
2025-05-20 22:25:28 +0100

    Added the voltar button

--- MindSpace/src/mindspace/controller/SaudeController.java
+++ MindSpace/src/mindspace/controller/SaudeController.java
@@ -4,9 +4,16 @@
  */
 package mindspace.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +22,21 @@ import javafx.fxml.Initializable;
  */
 public class SaudeController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private Button btnVoltar;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    @FXML
+    private void voltarParaMenuPrincipal() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mindspace/view/MainMenu.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btnVoltar.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
